b92d753110c70c598e8a51e65e307739411c2c9b
David Zim <davidzim29.90@gmail.com>
2023-01-06 11:51:59 +0200

    change products to be taken from DB and not from fakestore api

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,10 +1,12 @@
 const asyncHandler = require('../middlewares/asyncHandler');
-const fetch = (...args) =>
-    import('node-fetch').then(({default: fetch}) => fetch(...args));
+const Product = require('../models/productSchem');
+// const fetch = (...args) =>
+//     import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-exports.fetchProducts = asyncHandler( (req, res) => {
-    fetch(  "https://fakestoreapi.com/products")
-        .then(res => res.json())
-        .then(response => res.json(response))
-        .catch(console.log);
+exports.fetchProducts = asyncHandler( async (req, res) => {
+
+   const products = await Product.find({});
+
+   res.json(products);
+
 });
